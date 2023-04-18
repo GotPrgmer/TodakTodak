c0154cdc7595504f4b541e14b7171dc24920ad72
김민준 <gofiction95@gmail.com>
2023-04-18 09:41:53 +0900

    [Add] 프로젝트 생성
    Former-commit-id: f79984cc97e4ac0584062a942955b671ec4dcb1e

---
+++ BackEnd/src/main/java/com/ssafy/todaktodak/TodaktodakApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.todaktodak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaktodakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaktodakApplication.class, args);
+	}
+
+}
