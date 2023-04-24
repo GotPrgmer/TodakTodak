03d1708c1d396086c36e8325417c627428a552e6
kwanseob <foreverdunk@naver.com>
2023-04-24 16:59:41 +0900

    Feat: Folder_Structuralization
    Former-commit-id: cad47641a67f9dadd9c80ecc5112938e050499a7

---
+++ front/lib/pages/myPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('마이 페이지');
+  }
+}
---
+++ front/lib/pages/video.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Video extends StatelessWidget {
+  const Video({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('비디오 페이지');
+  }
+}
