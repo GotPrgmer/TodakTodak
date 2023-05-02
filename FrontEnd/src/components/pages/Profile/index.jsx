dacfec3f5f927f2497b9255ea8f71b1c181149c8
kwanseob <foreverdunk@naver.com>
2023-05-02 21:28:26 +0900

    Feat: Home developing
    Former-commit-id: d263de6a791d1a443db4363ca4dcbbd25d7943ce

--- FrontEnd/src/components/pages/Profile/index.jsx
+++ FrontEnd/src/components/pages/Profile/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import TopBar from "../../organisms/TopBar";
 import BottomBar from "../../organisms/BottomBar";
+import BabyProfile from "../../organisms/Profile";
+// import Sensor from "../../organisms/Sensor";
+
+// import { useEffect } from "react";
+// import axios from "axios";
 
 function Profile() {
+
   return (
     <div>
-      <TopBar />
-      <BottomBar />
+        <TopBar />
+        <div className="pt-16">
+              <BabyProfile />
+              {/* <Sensor/> */}
+        </div>
+        <BottomBar />
     </div>
   );
 }
