e1e81d0cc4dc5f0cdf86936e267fd49707ef383a
김민준 <gofiction95@gmail.com>
2023-05-06 15:41:08 +0900

    [Update] OpenVidu 컨트롤러 위치 옮김, 디바이스 엔티티 수정
    Former-commit-id: ad005d27540c18074172a626ae9e27c41dea469d

--- BackEnd/src/main/java/com/ssafy/todaktodak/domain/device/domain/Device.java
+++ BackEnd/src/main/java/com/ssafy/todaktodak/domain/device/domain/Device.java
@@ -1,7 +1,14 @@
 package com.ssafy.todaktodak.domain.device.domain;
 
 import com.ssafy.todaktodak.domain.baby.domain.Baby;
+import com.ssafy.todaktodak.domain.baby.domain.Gender;
+import com.ssafy.todaktodak.domain.user.domain.User;
 import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -9,17 +16,23 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="device")
 public class Device {
 
     @Id
     @Column(name = "device_id")
+    @GeneratedValue
     private Integer deviceId;
 
     @OneToOne
     @JoinColumn(name = "baby_id")
     private Baby baby;
 
+    @NotNull
     @Column(name = "serial_number")
     private String serialNumber;
 
@@ -38,6 +51,13 @@ public class Device {
     @Column(name = "device_updated_date")
     @LastModifiedDate
     private LocalDateTime userUpdatedDate;
+
+    public static Device newDeviceCreate(Baby baby, String babyImage){
+        return Device.builder()
+                .baby(baby)
+                .serialNumber("todak"+baby.getBabyId())
+                .build();
+    }
 
 
 
