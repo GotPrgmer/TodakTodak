bfdb83455a4824e93f5b7532ec9c94fe35e5a7b3
김민준 <gofiction95@gmail.com>
2023-05-08 11:02:02 +0900

    [Update] 디바이스 조회 기능, api맨 앞에 붙이기
    Former-commit-id: ec00d874fcc8822aad6c5672408ddaa9df6fed46

---
+++ BackEnd/src/main/java/com/ssafy/todaktodak/domain/device/service/DeviceService.java
@@ -0,0 +1,38 @@
+package com.ssafy.todaktodak.domain.device.service;
+
+import com.ssafy.todaktodak.domain.baby.domain.Baby;
+import com.ssafy.todaktodak.domain.baby.dto.BabyInfoResponseDto;
+import com.ssafy.todaktodak.domain.baby.repository.BabyRepository;
+import com.ssafy.todaktodak.domain.device.domain.Device;
+import com.ssafy.todaktodak.domain.device.dto.DeviceInfoResponseDto;
+import com.ssafy.todaktodak.domain.device.repository.DeviceRepository;
+import com.ssafy.todaktodak.global.error.CustomException;
+import com.ssafy.todaktodak.global.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DeviceService {
+
+    private final DeviceRepository deviceRepository;
+
+
+    @Transactional
+    public DeviceInfoResponseDto deviceInfo(Integer babyId){
+        //babyId로 아기 조회
+        Optional<Device> device = deviceRepository.findByBabyBabyId(babyId);
+        if ( device.isEmpty()) {
+            throw new CustomException(ErrorCode.ENTITY_NOT_FOUND);
+        }
+        Device findDevice = device.get();
+
+        return DeviceInfoResponseDto.ofDevice(findDevice);
+        //
+    }
+}
