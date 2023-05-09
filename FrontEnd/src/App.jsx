2e2e6b6be70247ae401cc77caa138ada22deac0b
powerjjh95 <powerjjh95@gmail.com>
2023-05-09 13:56:53 +0900

    Fix : Debugging
    Former-commit-id: 39c0e97430dc7ae50f6d29332a19d84080e3fb14

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { RouterProvider } from "react-router-dom";
-import router from "./Router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./components/pages/Profile";
+import Video from "./components/pages/Video";
+import Cry from "./components/pages/Cry";
+import MyPage from "./components/pages/MyPage";
+import Edit from "./components/pages/Edit";
+import KakaoLogin from "./components/pages/Login";
+import Loading from "./components/pages/Loading";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/video" element={<Video />} />
+          <Route path="/cry" element={<Cry />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/login" element={<KakaoLogin />} />
+          <Route path="/api/login/oauth2/code/kakao" element={<Loading />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- front/lib/style.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-var theme = ThemeData(
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        backgroundColor: Colors.white,
-        elevation: 2,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey.withOpacity(.60)));
