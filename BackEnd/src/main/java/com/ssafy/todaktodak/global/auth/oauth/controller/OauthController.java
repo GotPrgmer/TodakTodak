e19481420be7f162d770f30b95401f52fccb7040
김민준 <gofiction95@gmail.com>
2023-05-12 07:42:28 +0900

    [Update] Redis 설정, refreshToken 재발급 로직
    Former-commit-id: a7605851cb9b190defc3258d3153c9afde58d2c2

--- BackEnd/src/main/java/com/ssafy/todaktodak/global/auth/oauth/controller/OauthController.java
+++ BackEnd/src/main/java/com/ssafy/todaktodak/global/auth/oauth/controller/OauthController.java
@@ -2,23 +2,28 @@ package com.ssafy.todaktodak.global.auth.oauth.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.todaktodak.global.auth.oauth.dto.LoginResponseDto;
-import com.ssafy.todaktodak.global.auth.oauth.service.KakaoService;
+import com.ssafy.todaktodak.global.auth.oauth.service.OauthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class KakaoController {
-    private final KakaoService kakaoService;
+public class OauthController {
+    private final OauthService oauthService;
 
     @GetMapping("/login/oauth2/code/kakao")
     public ResponseEntity<LoginResponseDto> kakaoCallback(@RequestParam("code") String code) throws JsonProcessingException {
-        return kakaoService.verificationKakao(code);
+        return oauthService.verification(code);
+    }
+
+    @PostMapping("/token/reissue")
+    public ResponseEntity<?> tokenReissue(HttpServletRequest request) {
+
+        return oauthService.tokenReissue(request);
     }
 
 
