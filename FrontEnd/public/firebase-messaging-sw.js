50c59112f84d95d087c83ee03141af2403a1c84c
엄희원 <eom9300@hanmail.net>
2023-05-15 13:38:26 +0900

    fix:delete bg message fcm
    Former-commit-id: d1ebef3a8d603d985cd7ed5e976cea8dccedc8f3

--- FrontEnd/public/firebase-messaging-sw.js
+++ FrontEnd/public/firebase-messaging-sw.js
@@ -63,17 +63,17 @@ const app = firebase.initializeApp(firebaseConfig);
 // 성공 Code
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
-  };
+// messaging.onBackgroundMessage(function (payload) {
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+//   // Customize notification here
+//   const notificationTitle = "Background Message Title";
+//   const notificationOptions = {
+//     body: "Background Message body.",
+//     icon: "/firebase-logo.png",
+//   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
