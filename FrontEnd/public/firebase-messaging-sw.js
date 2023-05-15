f5b51c9e21d127b97cbfdba09ad825d837251d41
powerjjh95 <powerjjh95@gmail.com>
2023-05-15 17:52:42 +0900

    Fix : FCM Test
    Former-commit-id: 655df295b7447e1b43055d30b1510279f2169ea8

--- FrontEnd/public/firebase-messaging-sw.js
+++ FrontEnd/public/firebase-messaging-sw.js
@@ -62,18 +62,3 @@ const app = firebase.initializeApp(firebaseConfig);
 // const messaging = firebase.messaging(app);
 // 성공 Code
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
