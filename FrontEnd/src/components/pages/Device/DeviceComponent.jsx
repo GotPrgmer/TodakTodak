4a78e3a1e3ae62d78dae427f4aed8b4bee052fa1
김이삭 <isakggong@gmail.com>
2023-05-16 20:33:32 +0900

    Add feat/FE/AIImage
    새로운 학습 모델 및 이벤트 스로틀링 적용 #4
    
    
    Former-commit-id: ec331e94be21c9d328d20a93a5bf37defb83d2a7

--- FrontEnd/src/components/pages/Device/DeviceComponent.jsx
+++ FrontEnd/src/components/pages/Device/DeviceComponent.jsx
@@ -172,7 +172,7 @@ class DeviceComponent extends Component {
       const currentTime = new Date().getTime();
 
       if(this.state.lastAlarmTime || currentTime - this.state.lastAlarmTime >= this.state.throttleTime){
-        fetch(`https://todakaiot.com:8080/api/device/alarm`, {
+        fetch(`https://todaktodak.kr:8080/api/device/alarm`, {
           headers: {
             "Content-Type": "application/json",
           },
