1e838e30ced806667b219b7542426b0eb093d60d
김민준 <gofiction95@gmail.com>
2023-05-19 00:29:56 +0900

    [Fix] 404페이지
    Former-commit-id: 96009d827f49a2c07069c423294a1538f5686c69

--- FrontEnd/src/components/pages/NotFound/index.jsx
+++ FrontEnd/src/components/pages/NotFound/index.jsx
@@ -6,13 +6,14 @@ function NotFound() {
   return (
     <>
       <TopBar />
-      <div className="flex flex-col justify-center items-center mt-[10vh]">
+      <div className="flex flex-col justify-center items-center mt-[20vh]">
         <img
-          className="object-cover"
-          src="https://todak-s3-free-bucket.s3.ap-northeast-2.amazonaws.com/assets/DwAW1676947841_197213620.png"
+          className="w-[50%]"
+          src="https://todak-s3-free-bucket.s3.ap-northeast-2.amazonaws.com/assets/image-removebg-preview (15) 1.png"
           alt="asdf"
         />
-        <div className="font-bold text-xl">여기 맞아? 기기 번호는</div>
+        <div className="font-bold text-xl mb-1">잘못된 주소이거나</div>
+        <div className="font-bold text-xl">기기를 등록해주세요. </div>
       </div>
       <BottomBar />
     </>
