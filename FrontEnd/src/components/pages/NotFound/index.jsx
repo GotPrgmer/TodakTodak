d56a2d8e217aa435cb1762e3e98eab27d103bee5
김민준 <gofiction95@gmail.com>
2023-05-18 17:24:56 +0900

    [Update] 404페이지
    Former-commit-id: 61c59c5843c46ae5b96968586801c45180915015

--- FrontEnd/src/components/pages/NotFound/index.jsx
+++ FrontEnd/src/components/pages/NotFound/index.jsx
@@ -6,7 +6,14 @@ function NotFound() {
   return (
     <>
       <TopBar />
-      <div> 기기 번호가 일치하지 않습니다.</div>
+      <div className="flex flex-col justify-center items-center mt-[10vh]">
+        <img
+          className="object-cover"
+          src="https://todak-s3-free-bucket.s3.ap-northeast-2.amazonaws.com/assets/DwAW1676947841_197213620.png"
+          alt="asdf"
+        />
+        <div className="font-bold text-xl">여기 맞아? 기기 번호는</div>
+      </div>
       <BottomBar />
     </>
   );
